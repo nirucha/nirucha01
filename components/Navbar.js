d27c8fd8903c42eec63db3e777131265af4b0df0
Your Name <your.email@example.com>
2025-03-13 02:29:02 +0700

    ทำส่วนNavbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+// import "@/styles/Navbar.module.css";
+import styles from "@/styles/Navbar.module.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false); // state สำหรับการเปิด/ปิด dropdown
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen); // Toggle การเปิด/ปิด dropdown
+  };
   return (
     <nav>
-      <Link href={"/"}>
+      <Link href={"/"} className="logo-link">
         <Image src="/Cat-Memes-27.jpg" width={50} height={50} alt="Cat Meme" />
       </Link>
       <Link href={"/"}>หน้าแรก</Link>
       <Link href={"/about"}>เกี่ยวกับเรา</Link>
-      <Link href={"/products"}>สินค้า</Link>
+      <div className={styles.dropdown}>
+        <button className={styles.dropbtn}>ตัวอย่างผลงาน</button>
+        <div className={styles.dropdownContent}>
+          <Link href={"/products"}>Ex.01 การดึงAPIแสดงข้อมูลสินค้า</Link>
+        </div>
+      </div>
     </nav>
   );
 };
